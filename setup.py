3852411f4c6e91c0516b65f7363f0547187e5e14
Peter Fison <peter@southwestlondon.tv>
2020-11-15 07:20:14 +0000

    ~description

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import pathlib
 from setuptools import setup
-# from distutils.core import setup
 from AWSOMdesktop import __version__
-from AWSOMdesktop import __name__
 
+NAME = "AWSOMdesktop"
+URL = f'https://github.com/pfython/{NAME}'
 HERE = pathlib.Path(__file__).parent
 
 setup(
-  name = '__name__',
-  packages = ['__name__'],
+  name = 'NAME',
+  # packages = ['NAME'],
   version = __version__,
   license='MIT',
-  description = 'A data structure which allows both object attributes and dictionary keys and values to be used simultaneously and interchangeably.',
+  description = 'Desktop automation tools for managing common tasks such as renaming media files and making backups, as well as interacting with Adobe Premiere Pro using the `Pymiere` library (Windows only).',
   long_description=(HERE / "README.md").read_text(),
   long_description_content_type="text/markdown",
   author = 'Peter Fison',
   author_email = 'peter@southwestlondon.tv',
-  url = f'https://github.com/pfython/{__name__}',
-  download_url = f'{url}/archive/v_{__version__}.tar.gz',
-  keywords = [__name__, "search", "video", "audio", "media", "AWSOM", "TV", "content", "manage", "organise", "organize", "Premiere", "Adobe", "Pymiere", "YouTube", "metadata", "workflow", "automation", "Creative Cloud", "edit", "editor", "editing"],
+  url = URL,
+  download_url = f'{URL}/archive/v_{__version__}.tar.gz',
+  keywords = [NAME, "search", "video", "audio", "media", "AWSOM", "TV", "content", "manage", "organise", "organize", "Premiere", "Adobe", "Pymiere", "YouTube", "metadata", "workflow", "automation", "Creative Cloud", "edit", "editor", "editing"],
   install_requires=['cleverdict'],
   classifiers=[
     'Development Status :: 4 - Beta',
@@ -38,6 +38,8 @@ setup(
 )
 
 # Run the following from the command prompt:
+
+# python -m pip install setuptools wheel twine
 # python setup.py sdist
 # python -m twine upload --repository testpypi dist/*
 # python -m twine upload --repository pypi dist/*
